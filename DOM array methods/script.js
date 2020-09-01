de4319071c01e9b96a94d2d7fe0bfe4d6d42442b
rossmylesobrien <rossmylesobrien@gmail.com>
2020-09-01 22:36:43 +0100

    Adding DOM arrays

---
+++ DOM array methods/script.js
@@ -0,0 +1,30 @@
+const main = document.getElementById('main');
+const addUserBtn = document.getElementById('add-user');
+const double = document.getElementById('double');
+const showMillionairesBtn = document.getElementById('show-millionaires');
+const sortBtn = document.getElementById('sort');
+const calculateWealthBtn = document.getElementById('calculate-wealth');
+
+let data = [];
+
+// Fetch random user and add money
+async function getRandomUser(){
+  const res = await fetch('https://randomuser.me/api');
+  const data = await res.json();
+
+  console.log(data);
+  const user = data.results[0];
+
+  const newUser = {
+    name: `${user.name.first} ${user.name.last}`,
+    money: Math.floor(Math.random() * 1000000)
+  }
+  console.log(newUser);
+  addData(newUser);
+}
+
+function addData(obj){
+    data.push(obj);
+}
+
+getRandomUser();
