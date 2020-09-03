230da520ec991b210187e23435bb0740079aedec
rossmylesobrien <rossmylesobrien@gmail.com>
2020-09-03 20:43:18 +0100

    Add the Double money feature

--- DOM array methods/script.js
+++ DOM array methods/script.js
@@ -1,6 +1,6 @@
 const main = document.getElementById('main');
 const addUserBtn = document.getElementById('add-user');
-const double = document.getElementById('double');
+const doubleBtn = document.getElementById('double');
 const showMillionairesBtn = document.getElementById('show-millionaires');
 const sortBtn = document.getElementById('sort');
 const calculateWealthBtn = document.getElementById('calculate-wealth');
@@ -12,14 +12,12 @@ async function getRandomUser(){
   const res = await fetch('https://randomuser.me/api');
   const data = await res.json();
 
-  console.log(data);
   const user = data.results[0];
 
   const newUser = {
     name: `${user.name.first} ${user.name.last}`,
     money: Math.floor(Math.random() * 1000000)
   }
-  console.log(newUser);
   addData(newUser);
 }
 
@@ -43,6 +41,14 @@ function updateDom(providedData = data){
     });
 };
 
+// Double money
+function doubleMoney(){
+    data = data.map(user => {
+      return {...user, money: user.money * 2}
+    });
+    updateDom();
+};
+
 // Format number as money
 function formatMoney(number){
   return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
@@ -50,3 +56,5 @@ function formatMoney(number){
 
 // Event listeners
 addUserBtn.addEventListener('click', getRandomUser);
+
+doubleBtn.addEventListener('click', doubleMoney);
