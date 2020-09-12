60678a239d0b24d6f7da65fbc79025e10357c1de
rossmylesobrien <rossmylesobrien@gmail.com>
2020-09-12 20:26:42 +0100

    Add the final meal finder functionality

--- meal_finder/script.js
+++ meal_finder/script.js
@@ -30,7 +30,7 @@ function searchMeal(e){
               mealsEl.innerHTML = data.meals.map(meal => `
                 <div class="meal">
                   <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
-                  <div class="meal-info" data-mealID=${meal.idMeal}>
+                  <div class="meal-info" data-mealID="${meal.idMeal}">
                     <h3>${meal.strMeal}</h3>
                   </div>
                 </div>`).join('');
@@ -47,7 +47,7 @@ function searchMeal(e){
 // Fetch meal by ID
 function getMealById(mealID){
   fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
-  .then(res => res.json)
+  .then(res => res.json())
   .then(data => {
     const meal = data.meals[0];
 
@@ -55,7 +55,24 @@ function getMealById(mealID){
   })
 };
 
-// Add meal to document
+// Fetch random meal from API
+function getRandomMeal(){
+
+// Clear meals and heading
+mealsEl.innerHTML = '';
+resultHeading.innerHTML = '';
+
+fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
+  .then(res => res.json())
+  .then(data => {
+    const meal = data.meals[0];
+
+    addMealToDOM(meal);
+
+  });
+};
+
+// Add meal to DOM
 function addMealToDOM(meal){
   const ingredients = [];
 
@@ -70,12 +87,26 @@ function addMealToDOM(meal){
   single_mealEl.innerHTML = `
   <div class="single-meal">
     <h1>${meal.strMeal}</h1>
+    <img src="${meal.strMealThumb}" alt="${meal.strMeal}"/>
+    <div class="single-meal-info">
+        ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ""}
+        ${meal.strArea ? `<p>${meal.strArea}</p>` : ""}
+    </div>
+    <div class="main">
+      <p>${meal.strInstructions}</p>
+      <h2>Ingredients</h2>
+      <ul>
+        ${ingredients.map(ing => `<li>${ing}</li>`).join('')}
+      </ul>
+    </div>
   </div>
   `;
 }
 
 // Event Listener
 submit.addEventListener('submit', searchMeal);
+random.addEventListener('click', getRandomMeal);
+
 
 mealsEl.addEventListener('click', e => {
     const mealInfo = e.path.find(item => {
@@ -84,7 +115,7 @@ mealsEl.addEventListener('click', e => {
       }else{
         return false;
       }
-    })
+    });
 
     if(mealInfo){
         const mealID = mealInfo.getAttribute('data-mealid');
