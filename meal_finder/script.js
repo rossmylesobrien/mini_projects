f47193a23a93aaa1bca01915157740c34f61f62f
rossmylesobrien <rossmylesobrien@gmail.com>
2020-09-12 19:52:21 +0100

    Adding the meal description sections

--- meal_finder/script.js
+++ meal_finder/script.js
@@ -44,5 +44,50 @@ function searchMeal(e){
 
 }
 
+// Fetch meal by ID
+function getMealById(mealID){
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
+  .then(res => res.json)
+  .then(data => {
+    const meal = data.meals[0];
+
+    addMealToDOM(meal);
+  })
+};
+
+// Add meal to document
+function addMealToDOM(meal){
+  const ingredients = [];
+
+  for(let i = 1; i <= 20; i++){
+      if(meal[`strIngredient${i}`]){
+          ingredients.push(`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`);
+      }else{
+          break;
+    }
+  }
+
+  single_mealEl.innerHTML = `
+  <div class="single-meal">
+    <h1>${meal.strMeal}</h1>
+  </div>
+  `;
+}
+
 // Event Listener
 submit.addEventListener('submit', searchMeal);
+
+mealsEl.addEventListener('click', e => {
+    const mealInfo = e.path.find(item => {
+      if(item.classList){
+          return item.classList.contains('meal-info');
+      }else{
+        return false;
+      }
+    })
+
+    if(mealInfo){
+        const mealID = mealInfo.getAttribute('data-mealid');
+        getMealById(mealID);
+    }
+});
