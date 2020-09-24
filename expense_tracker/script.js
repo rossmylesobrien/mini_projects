7fcfe35987feb0fef0bbfdec198f30fd1433554d
rossmylesobrien <rossmylesobrien@gmail.com>
2020-09-24 12:17:47 +0100

    Adding the add & delete transactions

--- expense_tracker/script.js
+++ expense_tracker/script.js
@@ -15,6 +15,36 @@ const dummyTransactions = [
 
 let transactions = dummyTransactions;
 
+
+// Add transaction
+function addTransation(e){
+  e.preventDefault() // To repvent it from submitting
+
+  if(text.value.trim() === '' || amount.value.trim() === ''){
+      alert('Please add a text and amount');
+  }else{
+    const transaction = {
+        id: generateID(),
+        text: text.value,
+        amount: +amount.value
+    };
+
+    transactions.push(transaction);
+    addTransactionDOM(transaction);
+
+    updateValues();
+
+    text.value = '';
+    amount.value = '';
+  }
+}
+
+// Genertate random ID
+
+function generateID(){
+  return Math.floor(Math.random() * 100000000);
+};
+
 // Add transactions to DOM list
 function addTransactionDOM(transaction){
   // Get sign
@@ -26,7 +56,7 @@ function addTransactionDOM(transaction){
 
   item.innerHTML = `
     ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span>
-    <button class="delete-btn">X</button>
+    <button class="delete-btn" onclick="removeTransaction(${transaction.id})">X</button>
   `;
 
   list.appendChild(item);
@@ -47,11 +77,20 @@ function updateValues(){
 
 };
 
+// Remove transaction by ID
+function removeTransaction(id){
+    transactions = transactions.filter(transaction => transaction.id !== id);
+
+    init();
+};
+
 // Init the app
 function init(){
   list.innerHTML = '';
   transactions.forEach(addTransactionDOM);
 };
-init();
 
+form.addEventListener('submit', addTransation);
+
+init();
 updateValues();
