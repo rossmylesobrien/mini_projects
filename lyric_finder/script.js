b55bbfd2913c5a168eb2d74f982e0b4b713de191
rossmylesobrien <rossmylesobrien@gmail.com>
2020-09-26 14:16:54 +0100

    Adding the lyrics

--- lyric_finder/script.js
+++ lyric_finder/script.js
@@ -76,3 +76,30 @@ form.addEventListener('submit', e => {
       searchSongs(searchTerm);
     }
 });
+
+// get lyrics button click
+result.addEventListener('click', e => {
+  const clickedEl = e.target;
+
+  if(clickedEl.tagName === 'BUTTON'){
+    const artist = clickedEl.getAttribute('data-artist');
+    const songTitle = clickedEl.getAttribute('data-songtitle');
+
+    getLyrics(artist, songTitle);
+  }
+});
+
+// Get lyrics for song
+async function getLyrics(artist, songTitle){
+  const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
+  const data = await res.json();
+
+  const lyrics = data.lyrics.replace(/(\r\n)|\r|\n/g, '<br>');
+
+  result.innerHTML = `
+  <h2><strong>${artist}</strong> - ${songTitle}</h2>
+  <span>${lyrics}</span>
+  `;
+
+  more.innerHTML = '';
+}
