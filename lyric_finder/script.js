19082424a63c1d0679c1918dead94251a86d61eb
rossmylesobrien <rossmylesobrien@gmail.com>
2020-09-26 14:05:33 +0100

    Adding the pagination

--- lyric_finder/script.js
+++ lyric_finder/script.js
@@ -38,31 +38,29 @@ function showData(data){
   result.innerHTML = `
     <ul class="songs">
       ${data.data
-        .map(
-          song => `<li>
-      <span><strong>${song.artist.name}</strong> - ${song.title}</span>
-      <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
-    </li>`
-        )
-        .join('')}
+        .map(song => `
+      <li>
+        <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+        <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+      </li>`
+        ).join('')}
     </ul>
   `;
 
-  if(data.prev || data.next){
-      more.innerHTML = `
-        ${data.prev ? `<button class="btn" onclick="getMoreSongs('${data.prev}')">Prev</button>` : ""}
-        ${data.next ? `<button class="btn" onclick="getMoreSongs('${data.next}')>Next</button>` : ""}
-        `;
-  }else{
-    more.innerHTML = "";
+  if (data.prev || data.next) {
+    more.innerHTML = `
+      ${data.prev ? `<button class="btn" onclick="getMoreSongs('${data.prev}')">Prev</button>` : ''}
+      ${data.next ? `<button class="btn" onclick="getMoreSongs('${data.next}')">Next</button>` : ''}
+    `;
+  } else {
+    more.innerHTML = '';
   }
 };
 
 // Get prev and next results
 async function getMoreSongs(url){
-  const res = await fetch(url);
+  const res = await fetch(`https://cors-anywhere.herokuapp.com/${url}`);
   const data = await res.json();
-
   showData(data);
 }
 
